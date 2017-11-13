8e9840d5f41cc84a1d1f26a25e7771f8ffc1afdf
Jason Ish <ish@unx.ca>
2017-11-13 14:30:29 +0100

    Use labels instead of tags...
    - command to list all names
    - command to list all labels

--- appid.py
+++ appid.py
@@ -26,27 +26,54 @@ grab the generated .rules files.
 
 import sys
 import os
+import argparse
 
 import yaml
 
 SID = 300000000
 
-def print_tls_sni(config):
+LABELS = {}
+NAMES = {}
+
+def print_tls_sni(output, config):
     global SID
 
     template = """alert tls any any -> any any (msg:"%(msg)s"; tls_sni; content:"%(content)s"; flow:to_server,established; flowbits:set,%(flowbit)s; sid:%(sid)d; rev:1;)"""
 
     for app in config:
-        for pattern in app["patterns"]:
-            print(template % {
-                "msg": "APPID TLS SNI Pattern for %s" % (app["flowbit"]),
-                "content": pattern,
-                "flowbit": app["flowbit"],
-                "sid": SID,
-            })
-            SID += 1
 
-def print_http_user_agent(config):
+        flowbits = []
+
+        if isinstance(app["flowbit"], list):
+            flowbits = app["flowbit"]
+        else:
+            flowbits.append(app["flowbit"])
+
+        for flowbit in flowbits:
+
+            if flowbit.startswith("service/label"):
+                tag = flowbit.split("/")[-1]
+                if not tag in LABELS:
+                    print("error: unknown tag: %s" % (tag), file=sys.stderr)
+            elif flowbit.startswith("service/name"):
+                name = flowbit.split("/")[-1]
+                if not name in NAMES:
+                    print("error: unknown name: %s" % (name), file=sys.stderr)
+            else:
+                print(
+                    "warning: unknown flowbit prefix: %s" % (flowbit),
+                    file=sys.stderr)
+
+            for pattern in app["patterns"]:
+                print(template % {
+                    "msg": "APPID TLS SNI Pattern for %s" % (flowbit),
+                    "content": pattern,
+                    "flowbit": flowbit,
+                    "sid": SID,
+                }, file=output)
+                SID += 1
+
+def print_http_user_agent(output, config):
     global SID
 
     template = """alert http any any -> any any (msg:"%(msg)s"; content:"%(content)s"; http_user_agent; flow:to_server,established; flowbits:set,%(flowbit)s; sid:%(sid)d; rev:1;)"""
@@ -58,16 +85,20 @@ def print_http_user_agent(config):
                 "content": pattern,
                 "flowbit": app["flowbit"],
                 "sid": SID,
-            })
+            }, file=output)
         SID += 1
 
-def main():
+def generate_rules(args):
+    if args.output:
+        output = open(args.output, "w")
+    else:
+        output = sys.stdout
 
     # Print out the license.
     with open("LICENSE.template") as fileobj:
         lines = fileobj.readlines()
         lines = ["# %s" % line for line in lines]
-        print("".join(lines))
+        print("".join(lines), file=output)
 
     for dirpath, dirnames, filenames in os.walk("."):
         for filename in filenames:
@@ -75,13 +106,60 @@ def main():
                 path = os.path.join(dirpath, filename)
                 with open(path) as fileobj:
                     config = yaml.load(fileobj)
+                    if "labels" in config:
+                        LABELS.update(config["labels"])
+                    if "names" in config:
+                        NAMES.update(config["names"])
                     for key in config:
                         if key == "tls-sni-patterns":
-                            print_tls_sni(config[key])
+                            print_tls_sni(output, config[key])
                         elif key == "http-user-agent-patterns":
-                            print_http_user_agent(config[key])
-                        else:
-                            raise Exception("Unknown appid type: %s" % (key))
+                            print_http_user_agent(output, config[key])
+
+def load_configs():
+    configs = []
+    for dirpath, dirnames, filenames in os.walk("."):
+        for filename in filenames:
+            if filename.endswith(".yaml"):
+                path = os.path.join(dirpath, filename)
+                configs.append(yaml.load(open(path)))
+    return configs
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output", metavar="<filename>",
+                        help="Output filename for rules")
+    parser.add_argument("command", metavar="<command>",
+                        help="Command to run")
+    args = parser.parse_args()
+
+    if args.command in ["gen", "generate"]:
+        generate_rules(args)
+    elif args.command == "list-labels":
+        labels = set()
+        configs = load_configs()
+        for config in configs:
+            for key in config:
+                if key == "tls-sni-patterns":
+                    for label in config[key]:
+                        for flowbit in label["flowbit"]:
+                            if flowbit.startswith("service/label"):
+                                labels.add(flowbit)
+        for label in labels:
+            print(label)
+    elif args.command == "list-names":
+        names = set()
+        configs = load_configs()
+        for config in configs:
+            for key in config:
+                if key == "tls-sni-patterns":
+                    for label in config[key]:
+                        for flowbit in label["flowbit"]:
+                            if flowbit.startswith("service/name"):
+                                names.add(flowbit)
+        for name in names:
+            print(name)
 
 if __name__ == "__main__":
     sys.exit(main())
